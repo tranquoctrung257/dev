7b4ce937c7c8699c00fbd916e89b09fa028448d1
tranquoctrung257 <tt3617089@gmail.com>
2025-08-12 19:17:05 +0700

    update

--- python/2/1.py
+++
@@ -1,4 +0,0 @@
-# link https://oj.vnoi.info/problem/fct050_sumpart
-n = int(input())
-if n%2==0: print("YES")
-else: print("NO")
--- python/3/1.py
+++
@@ -1,6 +0,0 @@
-s = input()
-
-for i in s:
-    if (i >= chr(65) and i <= chr(90)) or (i >= chr(97) and i <= chr(122)):
-        print(i,end='')
-
