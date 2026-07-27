c35981049e2c9466996dfd8c49fd75e9f02e5894
tranquoctrung257 <tt3617089.gmail.com>
2026-07-27 13:22:25 +0700

    update

---
+++ python/YT_Lập_Trình_Python/2026/day 1/1.py
@@ -0,0 +1,30 @@
+# cài python 
+
+# để kiểm tra trong máy có bao nhiêu phiên bản python thì gõ py -0p
+# hiện dấu * là phiên bản được máy ưu tiên
+
+# để in ra cái j đó thì dùng hàm print
+
+print("Hello world")
+
+# để in nhiều thứ trên cần 1 dòng thì cần ngăn cách nhau bằng dấu ,
+
+print("Hello world", "xin chào")
+
+# để thay dấu cách bằng dấu j đó chúng ta dùng hàm đối số sep
+
+print(1,2,3,4,5,sep="*",end=" ")
+
+# để thay ký tự xuống dòng \n thành ký tự khác ta dùng đối số end
+
+print(6)
+
+# \t là 1 tab
+
+# biến lưu trữ dữ giá trị để sử dụng sau này, sử dụng các nơi khác trong chương trình của chúng ta 
+
+x = 5 # gán giá trị 5 cho biến x 
+
+# để in ra kiểu biến 
+
+print(type(x)) # int kiểu integer kiểu số nguyên 
